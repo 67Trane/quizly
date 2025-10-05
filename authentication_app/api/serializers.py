6a27c60036625903b5ecf56eff51c1d775300d0c
Trane <Trane_67@hotmail.de>
2025-10-05 20:42:54 +0200

    registration

--- authentication_app/api/serializers.py
+++ authentication_app/api/serializers.py
@@ -6,22 +6,24 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     confirmed_password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password", "confirmed_password"]
-        extra_kwargs = {"password": {"write_only": True}}
 
     def validate(self, attrs):
-        if attrs["password"] != attrs["confirmed_password"]:
-            raise serializers.ValidationError({"password": "Passwörter stimmen nicht überein."})
-        validate_password(attrs["password"])
+        password = attrs.get("password")
+        confirmed = attrs.get("confirmed_password")
+
+        if password != confirmed:
+            raise serializers.ValidationError({"confirmed_password": "Passwörter stimmen nicht überein."})
+
+        validate_password(password)
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop("confirmed_password")
+        validated_data.pop("confirmed_password", None)
         password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        user = User.objects.create_user(password=password, **validated_data)
         return user
--- authentication_app/api/views.py
+++ authentication_app/api/views.py
@@ -3,17 +3,30 @@ from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import status
 
 from django.conf import settings
 from .serializers import (
-    RegistrationSerializer,
-    LoginSerializer,
-    PasswordResetConfirmSerializer,
-    PasswordResetRequestSerializer,
+    RegisterSerializer,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
+access_token_time = 60*5     #  5 minutes
+refresh_token_time = 60 * 60 * 24   #  1 day
+
+
 class RegistrationView(APIView):
-    permission_classes= [AllowAny]
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"detail": "User created successfully!"}, status=status.HTTP_201_CREATED)
     
+    
+# class LoginView(APIView):
+#     permission_classes = [AllowAny]
+#     authentication_classes = []
